356be2de4eb0362c42075dd734717a8897b55073
vinaymeruva <vinaymeruva@gmail.com>
2023-05-26 17:40:58 +0530

    ROBOSHOP FRONTEND FILE ADED

--- Frontend.sh
+++ Frontend.sh
@@ -10,7 +10,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp files/NGINX-ROBOSHOP.CONF /etc/nginx/default.d/roboshop.conf
+cp files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 
 systemctl restart nginx
